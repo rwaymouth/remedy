982bd4bbaaf6902104811e7c7e2d74515a417d7b
Rob Waymouth <rob.waymouth@mcmurrytmg.com>
2015-07-09 09:09:23 -0400

    Tweaked local storage handleing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,21 @@
 /* jshint devel:true */
-var location_markup = '{{#each location}}<tr class="location--entry"><td> {{street}}</td><td>{{city}}</td><td>{{zip}}</td></tr>{{/each}}';
+var location_markup = '{{#each location}}<tr class="location--entry"><td class="location--street">{{street}}</td><td>{{city}}</td><td>{{zip}}</td><td><button class="location--remove">Delete</button></td></tr>{{/each}}';
 var template = Handlebars.compile(location_markup);
 
-var req = $.get('../data/data.json', function(json) {
-  appendLocation(json['locations']);
-});
+// var req = $.get('../data/data.json', function(json) {
+//   appendLocation(json['locations']);
+// });
 
-var locationData = {
-  "locations": [
+if (localStorage.locations) {
+  var locationData = JSON.parse(localStorage.locations);
+} else {
+  var locationData = {
+    "locations": [
 
-  ]
-};
+    ]
+  };
+}
+appendLocation(locationData['locations']);
 
 Object.observe(locationData.locations, function() {
   saveData(locationData);
@@ -18,6 +23,7 @@ Object.observe(locationData.locations, function() {
 
 
 function saveData(data) {
+  localStorage.locations = JSON.stringify(data);
   $.post("../data/data.json",
     data,
     function(err) {
@@ -43,7 +49,27 @@ $('#modal--submit').on('click', function() {
     "city": $('#cityState').val(),
     "zip": $('#zip').val()
   }
-    locationData.locations.push(data);
-    appendLocation([data]);
-    $('#locations table').table();
-})
+  locationData.locations.push(data);
+  appendLocation([data]);
+  $('#locations table').table();
+});
+
+$('.location--remove').on('click', function() {
+  var entry = $(this).parents('.location--entry');
+  var key = $('.location--street > .tablesaw-cell-content', entry).html();
+  var data = locationData.locations;
+  var filtered = data.filter(function(entry) {
+    if (entry['street'] === key) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  locationData.locations = filtered;
+  entry.remove();
+  if (locationData.locations.length > 0) {
+    localStorage.locations = JSON.stringify(filtered);
+  } else {
+    localStorage.clear(locations);
+  }
+});
